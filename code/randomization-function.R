5f471a52f5e7a49905e6eafc64b9920321f469cb
Dom <dom.scruton@hotmail.com>
2022-01-02 13:14:32 +0000

    Argument testing

--- code/randomization-function.R
+++ code/randomization-function.R
@@ -1,28 +1,102 @@
-randomFun <- function(data, K){
-  #Function to calculate randomized test statistics
-  #Inputs:
-  # data: combined dataset for the two samples
-  # K: number of randomization samples to carry out
-  #Output:
-  # p-value from the empirical t-distribution
+
+# randomFunTest -----------------------------------------------------------
+
+# Function to carry out testing of randomFun arguments (improves readability)
+
+randomFunTest(data, K, colTest, colResponse, levels){
+  # Check arguments
+
+  # 2) data argument
+  if (!is.data.frame(data) & !is.data.table(data)) {
+    stop("data must be of class data.frame or data.table")
+  }else if (length(sapply(data, is.numeric)) < 2) {
+    stop("Randomization test requires at least 2 numeric columns in input 
+         dataset")
+  }else if (length(sapply(data, is.numeric)) > 2 & is.null(vars)) {
+    warning("More than 2 columns provided and column names not specified. 
+            First two numeric columns used. Please specify required columns if 
+            these are incorrect")
+  }
+  # 3) K argument
+  if (!is.numeric(K)) {
+    stop("K (number of permutations) must be of type numeric or integer")
+  }else if (K <= 0 | K %% 1 != 0) {
+    stop("K must be a positive integer")
+  }
+  # 1) Vars argument
+  for (i in c("colTest", "colResponse")) {
+    if (!is.null(eval(parse(text = i)))) {
+      print(eval(parse(text = i)))
+      if (is.character(eval(parse(text = i))) & !(i %in% colnames(data))) {
+        stop("input data.table does not contain specified columns")
+      }
+    }
+
+    
+  }
+
+  }# 4) Levels argument
+  if (!is.null(levels)) {
+    if (condition) {
+      
+    }
+  }
+  if (length(unique(levels)) != 2) {
+    warning
+  }
+}
   
-  #Calculate the number of observations within each region
+  
+
+# randomFun ---------------------------------------------------------------
+
+
+
+randomFun <- function(data, K = 1000, colTest = NULL, colResponse = NULL, 
+                      levels = NULL){
+  # Function to calculate randomized test statistics
+  # Inputs:
+    # data: combined dataset for the two samples
+    # K: number of randomization samples to carry out
+    # colTest: optional character string specifying testing column
+    # colResponse: optional character string specifying response column
+    # levels: optional character vector of 2 levels for comparison
+  # Output:
+    # p-value from empirical t-distribution
+
+  # Check arguments
+  randomFunTest()
+  
+  # Convert dataframe to data.table (data.table more efficient)
+  data <- as.data.table(data)
+  # specify variables and levels if unspecified
+  for (i in c("colTest", "colResponse")) {
+    if (is.null(eval(parse(i)))) {
+      parse(i) <- 
+    }
+  }
+  
+  # Calculate number of observations within each level
   n1 <- length(which(data$region == "South"))
   n2 <- length(which(data$region == "Northeast"))
-  #Initialize vector of test statistics
+  # Initialize storage vector for test statistics
   Trand <- rep(NA, K)
-  #set seed for reproducibility
-  set.seed(160001695)
   for (k in 1:K) {
-    #Create randomised samples by permuting the data
+    # Create randomised samples by permuting the data
     indices <- sample(1:(n1 + n2))
     perm <- data$rate[indices]
-    #Calculate a t-statistic for each permutation
+    # Calculate t-statistic for each permutation
     Trand[k] <- abs(mean(perm[1:n1]) - mean(perm[(n1 + 1):(n1 + n2)]))
   }
-  #Calculate the t-statistic for the original data
+  # Calculate t-statistic for original data
   Tobs <- abs(mean(data$rate[1:n1]) - mean(data$rate[(n1 + 1):(n1 + n2)]))
-  #Calculate the p-value
+  # p-value
   p <- length(which(Trand > Tobs)) / K
   return(p)
+  
+  # Adding MetaData explaining function
+  attr(randomFun, "help") <- 
+    "randomFun carries out a Two-Sample Randomization test, using one numeric 
+    column and one categorical or numeric column consisting of two levels for 
+    comparison"
 }
