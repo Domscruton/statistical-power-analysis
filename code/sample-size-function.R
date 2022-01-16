a16d11e21af17dd99515a27da29fa88a786b88c2
Dom <dom.scruton@hotmail.com>
2022-01-16 20:15:54 +0000

    Removing diffMeans and gamma functions and moving their functionality into a single function, simulationFun
    Makes code more readable and succinct

--- code/sample-size-function.R
+++ code/sample-size-function.R
@@ -1,28 +1,81 @@
-sampSizeFun <- function(N, K, R, alpha = 0.05){
-  #Function to calculate size and power varying sample sizes
-  #Inputs:
-  # N: sample size to simulate for each region
-  # K: number of randomization samples to calculate
-  # R: number of test replications
-  # alpha: significance level
-  #Output:
-  # Results: (4x1)- dataframe containing the size and power of the two tests
+
+pValueFun <- function(){
+  # double assignment ensures object available outside function
+  P[i, 1] <<- randomFun(data = dtNew, K = K, colTest = "xTest", 
+                     colResponse = "xResponse", test.args = FALSE, 
+                     level)
+  # Actual t-test p-value
+  P[i, 2] <<- t.test(H0[1:N, 2], H0[(N + 1):(2 * N), 2])$p.value
+}
+
+
+SimulationFun <- function(N = 1000, K = 1000, R = 100, alpha = 0.05, 
+                          distribution = "gaussian", diff.means = 0, 
+                          sd = 1, shape = NULL, rate = NULL){
+  # Function to calculate size and power varying sample sizes
+  # Inputs:
+    # N: sample size to simulate for each level
+    # K: number of randomization samples to calculate
+    # R: number of test replications
+    # alpha: significance level
+    # distribution: distribution from which to simulate data
+      # (can be either gaussian or gamma)
+    # diff.means: difference in means of 2 levels if gaussian
+    # sd: standard deviation for the gaussian distribution simulations
+    # shape: length 2 vector of gamma shape parameters for each level
+      # (only if gamma distribution specified)
+    # rate: length 2 vector of gamma rate parameters for each level
+    # (only if gamma distribution specified)  
+  # Output:
+    # Results: (4x1)- list containing the size and power of the two tests
   
-  #Create a matrix to store p-values from the tests
-  P <- matrix(data = NA, nrow = R, ncol = 4)
-  for (i in 1:R) {
-    #Iterate sample creation for given sample size
-    set.seed(i)
-    H0 <- rbind(data.frame(region = "South", rate = rnorm(N, 0.04417, 0.1)), 
-                data.frame(region = "Northeast", rate = rnorm(N, 0.04417, 0.1)))
-    H1 <- rbind(data.frame(region = "South", rate = rnorm(N, 0.04417, 0.1)), 
-                data.frame(region = "Northeast", rate = rnorm(N, 0.01848, 0.1)))
-    #Fill the matrix with 4 p-values for each iteration
-    P[i, 1] <- randomFun(H0, K)
-    P[i, 2] <- randomFun(H1, K)
-    P[i, 3] <- t.test(H0[1:N, 2], H0[(N + 1):(2 * N), 2])$p.value
-    P[i, 4] <- t.test(H1[1:N, 2], H1[(N + 1):(2 * N), 2])$p.value
+  # Create matrix to store p-values from tests
+  P <- matrix(data = NA, nrow = R, ncol = 2)
+  # Create data.table to store simulated datasets
+  # (pre-defining size reduces computational burden)
+  dtNew <- data.table(xTest = c(rep("A", N), rep("B", N)), 
+                      xResponse = NA)
+  if (distribution == "gaussian") {
+    if (!is.null(shape) | !is.null(rate)) {
+      warning("Shape and/or rate parameters unused since simulations fitted
+              from gaussian. Specify distribution = gamma if gamma simulations
+              required")
+    }
+    for (i in 1:R) {
+      # Iterate sample creation for given sample size
+      dtNew[, xResponse := ifelse(xTest == "A", rnorm(N, 0, sd), 
+                                  rnorm(N, diff.means, sd))]
+      # Randomization test p-value
+      P[i, 1] <- randomFun(data = dtNew, K = K, colTest = "xTest", 
+                           colResponse = "xResponse")
+      # Actual t-test p-value
+      P[i, 2] <- t.test(H0[1:N, 2], H0[(N + 1):(2 * N), 2])$p.value
+    }
+  }else if (distribution == "gamma") {
+    # If shape or rate not specified, create defaults
+    if (is.null(shape) | is.null(rate)) {
+      shape <- rep(1, 2)
+      rate <- shape
+      warning("gamma distribution specified but shape or rate parameters 
+              incomplete.\nEnsure both are numeric vectors of length 2, 
+              otherwise defaults of shape = 1, rate = 1 used")
+    }
+    for (i in 1:R) {
+      # Potentially think about the efficiency of this- does it 
+      # extract the rate and shape values for each observation 
+      # assess this via profiling and use of system.time
+      dtNew[, xResponse := ifelse(xTest == "A", 
+                                  rgamma(N, shape = shape[1], rate = rate[2]), 
+                                  rgamma(N, shape = shape[2], rate = rate[2]))]
+      x <- pValueFun()
+    }
+  }else{
+    stop("distribution must be either 'gaussian' or 'gamma'\n 
+         default is gaussian if no distribution argument set")
   }
-  #return the size and power for each test as a dataframe
+
+  # return size and power of each test
   return(as.data.frame(results(P, R, alpha)))
 }
+
+# question- is it quicker to store values as a matrix or a data.table in the above?
